e70e48eabee44e8e87f23b8b665415709798971d
ambervanharskamp <am.vanharskamp@gmail.com>
2021-05-23 20:17:28 +1000

    csv modules added

---
+++ PyPoll/main.py
@@ -0,0 +1,8 @@
+#Import the os module to create a file paths across operating systems
+import os
+
+#Import the module to read the CSV files
+import csv
+
+#Set the path to the csv file
+csvpath = os.path.join('resources', 'election_data.csv')
